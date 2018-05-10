bb6a6a3eb7d8e0b5f5466c92bf5f925f35eae98b
matthew-schultz <mschultz@hawaii.edu>
2018-05-10 11:50:33 -1000

    Update setup.sh

--- cassandra/setup.sh
+++ cassandra/setup.sh
@@ -1,6 +1,8 @@
 #install cassandra on a debian 9 google cloud instance and get cassandra config files from repository
 # curl https://raw.githubusercontent.com/matthew-schultz/ICS421-FINAL/master/cassandra/setup.sh | bash
 # curl https://raw.githubusercontent.com/matthew-schultz/ICS421-FINAL/master/cassandra/setup.sh | bash -s cassandra/cassandra.yaml
+# curl https://raw.githubusercontent.com/matthew-schultz/ICS421-FINAL/master/cassandra/setup.sh | bash -s /cassandra/10-138-0-3/cassandra.yaml
+
 CASS_YAML=$1
 
 echo "deb http://www.apache.org/dist/cassandra/debian 39x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
